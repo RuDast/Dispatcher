d6522aeb4ea95d8a4737cc069f60ba8db48dc599
Xsunr1seX <nikitataganov19@gmail.com>
2025-05-22 23:32:36 +0300

    FAQ added

--- src/ui/Scenes/FAQScene.h
+++ src/ui/Scenes/FAQScene.h
@@ -1,27 +1,44 @@
 #ifndef FAQSCENE_H
 #define FAQSCENE_H
+
 #include "../Scene.h"
 #include "../../utils/Button.h"
+#include <vector>
+#include <string>
 
-class FAQScene : public Scene{
+class FAQScene : public Scene {
 public:
     FAQScene();
-
     void setBackBtnCallback(const std::function<void()>& callback);
-
-    void handleInput(const sf::Event &event) override;
-
+    void handleInput(const sf::Event& event) override;
     void update(float deltaTime) override;
-
-    void render(sf::RenderWindow &window) override;
+    void render(sf::RenderWindow& window) override;
 
 private:
+    void initCrawlText();
+
     sf::Font font;
     std::function<void()> back_btn_callback_;
     std::vector<Button> btns_;
 
+    // Для эффекта "звёздных войн"
+    sf::Text crawlText;
+    float textPositionY;
+    float crawlSpeed = 50.f; // Пикселей в секунду
+    std::vector<std::string> crawlLines = {
+        "Frequently Asked Questions",
+        "",
+        "Q: How to play this game?",
+        "A: Use arrow keys to move and space to interact.",
+        "",
+        "Q: How to save progress?",
+        "A: Progress saves automatically between levels.",
+        "",
+        "Q: How to change controls?",
+        "A: Go to Settings from the main menu.",
+        "",
+        "May the Force be with you!"
+    };
 };
 
-
-
-#endif //FAQSCENE_H
+#endif // FAQSCENE_H
